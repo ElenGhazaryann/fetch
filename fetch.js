a83daab98055f31b2b4a8f28e3d451c4084f8db5
Elen <elen.ghazaryan6@gmail.com>
2022-02-10 01:11:55 +0400

    feat: replace button click with keyup

--- fetch.js
+++ fetch.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const search = document.querySelector(".search");
-const button = document.querySelector(".button");
+// const button = document.querySelector(".button");
 const flag = document.querySelector(".flag");
 const name = document.querySelector(".name");
 const capital = document.querySelector(".capital");
@@ -9,8 +9,9 @@ const population = document.querySelector(".population");
 
 console.log(flag);
 
-button.addEventListener("click", (el) => {
-  fetch(`https://restcountries.com/v3.1/name/${search.value}`)
+search.addEventListener("keyup", (el) => {
+  setTimeout(() =>{
+    fetch(`https://restcountries.com/v3.1/name/${search.value}`)
     .then((res) => res.json())
     .then((res) => {
       name.innerText = res[0].name.common;
@@ -18,4 +19,29 @@ button.addEventListener("click", (el) => {
       population.innerText = `${(res[0].population / 1000000).toFixed(1)} mln`;
       flag.src = res[0].flags.png;
     });
+  },1000)
 });
+
+
+
+
+
+// const search = document.querySelector(".search");
+// const button = document.querySelector(".button");
+// const flag = document.querySelector(".flag");
+// const name = document.querySelector(".name");
+// const capital = document.querySelector(".capital");
+// const population = document.querySelector(".population");
+
+// console.log(flag);
+
+// button.addEventListener("click", (el) => {
+//   fetch(`https://restcountries.com/v3.1/name/${search.value}`)
+//     .then((res) => res.json())
+//     .then((res) => {
+//       name.innerText = res[0].name.common;
+//       capital.innerText = res[0].capital;
+//       population.innerText = `${(res[0].population / 1000000).toFixed(1)} mln`;
+//       flag.src = res[0].flags.png;
+//     });
+// });
